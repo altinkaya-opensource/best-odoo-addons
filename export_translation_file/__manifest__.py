4455b7535890b5d4585e8d2d77cdd36f1007a016
yibudak <yibudak@gmail.com>
2024-04-19 09:35:58 +0300

    [IMP] 16.0 modules translated with DeepL

---
+++ export_translation_file/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Ahmet Yiğit Budak (https://github.com/yibudak)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Export Translation File",
+    "summary": "Allows developers to easily generate i18n files",
+    "description": """
+        This module allows developers to easily generate i18n files for their modules.
+    """,
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "Ahmet Yiğit Budak",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/ir_module_module_view.xml",
+    ],
+}
